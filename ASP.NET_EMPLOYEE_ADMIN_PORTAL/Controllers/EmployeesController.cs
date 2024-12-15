3ccdc8249e2cc761fb82e46a768ebe8d7ed53fb8
Muhammad Usman <shepard1706@gmail.com>
2024-12-16 02:02:01 +0500

    Add project files.

---
+++ ASP.NET_EMPLOYEE_ADMIN_PORTAL/Controllers/EmployeesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NET_EMPLOYEE_ADMIN_PORTAL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+    }
+}
---
+++ ASP.NET_EMPLOYEE_ADMIN_PORTAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using ASP.NET_EMPLOYEE_ADMIN_PORTAL.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ASP.NET_EMPLOYEE_ADMIN_PORTAL.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
+
+// shortcuts
+// ctor
+// prop
+// ctrl + .
