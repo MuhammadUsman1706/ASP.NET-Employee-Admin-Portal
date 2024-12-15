b9d2d910defcb2942b804101e2a79d3cbb7d4702
Muhammad Usman <shepard1706@gmail.com>
2024-12-16 03:18:24 +0500

    added get all, post, and get by id

--- ASP.NET_EMPLOYEE_ADMIN_PORTAL/Controllers/EmployeesController.cs
+++ ASP.NET_EMPLOYEE_ADMIN_PORTAL/Controllers/EmployeesController.cs
@@ -1,11 +1,54 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ASP.NET_EMPLOYEE_ADMIN_PORTAL.Data;
+using ASP.NET_EMPLOYEE_ADMIN_PORTAL.Models;
+using ASP.NET_EMPLOYEE_ADMIN_PORTAL.Models.Entities;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ASP.NET_EMPLOYEE_ADMIN_PORTAL.Controllers
 {
+    // localhost:xxxx/api/employees
     [Route("api/[controller]")]
     [ApiController]
     public class EmployeesController : ControllerBase
     {
+        private readonly ApplicationDbContext dbContext;
+
+        public EmployeesController(ApplicationDbContext dbContext) {
+            this.dbContext = dbContext;
+        }
+
+
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            var allEmployees = dbContext.Employees.ToList<Employee>();
+
+            return Ok(allEmployees);
+        }
+
+        [Route("{id:guid}")]
+        [HttpGet]
+        public IActionResult GetEmployeeById(Guid id)
+        {
+            var employee = dbContext.Find<Employee>(id);
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee(AddEmployeeDto addEmployeeDto)
+        {
+            var employeeEntity = new Employee() { 
+                Name = addEmployeeDto.Name, 
+                Email = addEmployeeDto.Email, 
+                Phone = addEmployeeDto.Phone,
+                Salary = addEmployeeDto.Salary
+            };
+
+            dbContext.Employees.Add(employeeEntity);
+            dbContext.SaveChanges();
+
+            return Ok(employeeEntity);
+        }
+
+
     }
 }
